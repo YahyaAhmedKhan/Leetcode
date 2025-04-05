627c4731df2a102f44cbf96e78ff66027388a137
Yahya Ahmed Khan <96627206+YahyaAhmedKhan@users.noreply.github.com>
2025-04-05 19:07:16 +0500

    Time: 351 ms (6.14%), Space: 18.5 MB (6.65%) - LeetHub

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/1218-lowest-common-ancestor-of-deepest-leaves.py
@@ -0,0 +1,55 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        def find_d(node):
+            if not node: return -1
+            return max(find_d(node.left), find_d(node.right)) + 1
+        
+        d  = find_d(root)
+        c = 0
+        def count(node, depth):
+            nonlocal d, c
+            if not node:
+                return
+            if depth < d:
+                count(node.left, depth + 1)
+                count(node.right, depth + 1)
+                return
+            if depth == d:
+                c += 1
+                return
+
+        count(root, 0)
+        ans = (-1, None)
+        print(d, c)
+
+        def helper(node, depth):
+            nonlocal ans
+            if not node: return 0
+            if depth == d:
+                val = 1
+            else:
+                val = helper(node.left, depth+1) + helper(node.right, depth+1)
+
+            print("node ", node)
+            print("depth ", depth)
+            print("val ", val)
+            if val == c:
+                ans = max ((depth, node), ans)
+            
+            return val
+        helper(root, 0)
+        return ans[1]
+
+
+
+        helper(root, 0)
+        return ans
+
+            
