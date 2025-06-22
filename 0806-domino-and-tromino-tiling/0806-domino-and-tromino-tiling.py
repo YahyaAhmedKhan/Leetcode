151b9dd77e2e5fdf66e342019cf50504e03b1f6c
Yahya Ahmed Khan <96627206+YahyaAhmedKhan@users.noreply.github.com>
2025-06-22 10:12:07 +0500

    Time: 7 ms (18.29%), Space: 19.1 MB (17.12%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.py
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.py
@@ -1,28 +1,28 @@
 class Solution:
     def numTilings(self, n: int) -> int:
 
-        dp = [[-1]*2 for _ in range(n+1)]
+        dp = [[-1, -1] for i in range(n)]
 
-        dp[0][0]= 1
-        dp[0][1]= 0
-        dp[1][0]= 2
-        dp[1][1]= 1
+        dp[0][0] = 1
+        dp[0][1] = 0
+        if n==1:
+            return 1
+        dp[1][0] = 2
+        dp[1][1] = 2
 
-        def helper(i, state):
+        def helper(i, spacesLeft):
+            if dp[i][spacesLeft]!=-1:
+                return dp[i][spacesLeft]
 
-            if dp[i][state]!=-1:
-                return dp[i][state]
-
-            if state == 0:
-                ans = 2*helper(i-1, 1) + helper(i-1, 0) + helper(i-2, 0)
-                dp[i][state] = ans %(1e9+7)
-                return int(dp[i][state])
+            if spacesLeft==0:
+                dp[i][spacesLeft] = helper(i-1, 0) + helper(i-1, 1) + helper(i-2, 0)
+                dp[i][spacesLeft]%=(1e9+7)
             else:
-                ans = helper(i-2, 0) + helper(i-1, 1)
-                dp[i][state] = ans % (1e9+7)
-                return int(dp[i][state])
+                dp[i][spacesLeft] = helper(i-1, 1) + 2*helper(i-2, 0)
+                dp[i][spacesLeft]%=(1e9+7)
+            return dp[i][spacesLeft]
 
-        return helper(n-1, 0)
+        ret = int(helper(n-1, 0))
 
-
+        return ret
         
