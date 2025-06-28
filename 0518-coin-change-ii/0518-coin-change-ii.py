2c1fac813b7709f9b1e684583f472d6967fe190a
Yahya Ahmed Khan <96627206+YahyaAhmedKhan@users.noreply.github.com>
2025-06-28 23:26:22 +0500

    Time: 291 ms (48.59%), Space: 60.2 MB (32.48%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -12,7 +12,7 @@ class Solution:
             dp[i][0] = 1
 
         for i in range(1, m):
-            for j in range(n+1):
+            for j in range(1, n+1):
                 
                 if coins[i]<=j:
                     dp[i][j] = dp[i-1][j] + dp[i][j-coins[i]]
