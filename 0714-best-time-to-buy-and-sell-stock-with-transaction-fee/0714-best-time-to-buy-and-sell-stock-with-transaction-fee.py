752921d34a6e55a5420485852b2edd3e27417915
Yahya Ahmed Khan <96627206+YahyaAhmedKhan@users.noreply.github.com>
2025-06-28 16:15:09 +0500

    Time: 541 ms (16.53%), Space: 45.6 MB (19.89%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -3,27 +3,23 @@ class Solution:
         
         dp = [[None, None] for i in range(len(prices))]
         n = len(prices)
-
-        dp[0][1] = -1*prices[0]
         dp[0][0] = 0
+        dp[0][1] = -prices[0]
+
         def helper(i, bought):
-            nonlocal fee
-            if dp[i][bought]!=None:
+            if dp[i][bought] is not None:
                 return dp[i][bought]
 
-            if not bought:
-                ans = max(helper(i-1, 0), helper(i-1, 1)+prices[i]-fee)
-                dp[i][bought] = ans
-
+            if bought:
+                dp[i][bought] = max(helper(i-1, 0)-prices[i], helper(i-1, 1))
             else:
-                ans = max(helper(i-1, 0)-prices[i], helper(i-1, 1))
-                dp[i][bought] = ans
-
+                dp[i][bought] = max(helper(i-1, 1)+prices[i]-fee, helper(i-1, 0))
 
             return dp[i][bought]
 
 
-        ans = helper(n-1, 0)
+        ans =  helper(n-1, 0)
+        print(dp)
         return ans
 
             
